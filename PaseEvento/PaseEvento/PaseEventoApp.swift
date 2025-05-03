12e64535b0285dc6ca4bf9efdf72ee8d6c9a264a
JohnnyCJ20 <johnnyjesuscj@gmail.com>
2025-05-03 16:57:25 -0500

    add SwiftUI project Ticket

---
+++ PaseEvento/PaseEvento/PaseEventoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaseEventoApp.swift
+//  PaseEvento
+//
+//  Created by user274797 on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PaseEventoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
