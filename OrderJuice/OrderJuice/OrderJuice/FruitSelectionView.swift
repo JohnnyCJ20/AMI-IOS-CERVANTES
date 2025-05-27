f22a2d5161c3125fab50c040415702b20fdd6461
JohnnyCJ20 <82158678+JohnnyCJ20@users.noreply.github.com>
2025-05-26 20:56:09 -0500

    Update FruitSelectionView.swift

--- OrderJuice/OrderJuice/OrderJuice/FruitSelectionView.swift
+++ OrderJuice/OrderJuice/OrderJuice/FruitSelectionView.swift
@@ -1,18 +1,45 @@
-//
-//  FruitSelectionView.swift
-//  OrderJuice
-//
-//  Created by user274797 on 5/26/25.
-//
-
 import SwiftUI
 
 struct FruitSelectionView: View {
+    var onSelect: (String) -> Void
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color(red: 1.0, green: 0.8, blue: 0.9), Color.pink]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+
+            VStack {
+                Spacer()
+
+                Text("Choose a flavour")
+                    .font(.title2)
+                    .bold()
+                    .foregroundColor(.white)
+
+                HStack(spacing: 40) {
+                    Button(action: {
+                        onSelect("🍓")
+                    }) {
+                        Text("🍓")
+                            .font(.system(size: 80))
+                    }
+
+                    Button(action: {
+                        onSelect("🍇")
+                    }) {
+                        Text("🍇")
+                            .font(.system(size: 80))
+                    }
+                }
+                .padding(.top, 30)
+
+                Spacer()
+            }
+        }
     }
 }
 
-#Preview {
-    FruitSelectionView()
-}
